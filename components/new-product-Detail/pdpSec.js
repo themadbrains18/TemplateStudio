0d19630eba1610d7dc530c44cfcf67d1ff1acc37
amit-mehta28 <amitmehta7973@gmail.com>
2023-05-26 15:26:37 +0530

    preview page dynamically change

--- components/new-product-Detail/pdpSec.js
+++ components/new-product-Detail/pdpSec.js
@@ -10,7 +10,7 @@ import popupCloseBtn from 'public/icons/popupCloseBtn.svg'
 
 
 // import Swiper core and required modules
-import { Navigation, Autoplay } from 'swiper';
+import { Navigation, Autoplay, Pagination } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -21,6 +21,10 @@ import 'swiper/css/autoplay';
 import Link from 'next/link'
 
 
+import "swiper/css/pagination";
+
+
+
 const PdpSec = ({ product }) => {
 
     console.log(product, "product");
@@ -28,28 +32,28 @@ const PdpSec = ({ product }) => {
     const [pdpborder, setPdpborder] = useState(0);
     const [preview, setPreview] = useState(false);
 
-    
+
 
     useEffect(() => {
-       
+
         let previewBtn = document.body.querySelector(".preview_btn");
         let previewCloseBtn = document.body.querySelector(".preview_close_btn");
 
-        previewBtn.addEventListener("click", ()=>{
-                document.body.style.overflowY="hidden"
-              document.querySelector("html").style.overflowY="hidden";
-                // document.body.previousElementSibling.previousElementSibling.style.overflowY="hidden"
+        previewBtn.addEventListener("click", () => {
+            document.body.style.overflowY = "hidden"
+            document.querySelector("html").style.overflowY = "hidden";
+            // document.body.previousElementSibling.previousElementSibling.style.overflowY="hidden"
         })
-        previewCloseBtn.addEventListener("click", ()=>{
-            document.body.style.overflowY="unset"
+        previewCloseBtn.addEventListener("click", () => {
+            document.body.style.overflowY = "unset"
             // document.body.previousElementSibling.previousElementSibling.style.overflowY="unset"
-            document.querySelector("html").style.overflowY="unset";
+            document.querySelector("html").style.overflowY = "unset";
         })
     }, [])
 
     return (
         <>
-        {/* <SideDrawer open={showSideDrawer} closed={sideDrawerClosedHandler}/> */}
+            {/* <SideDrawer open={showSideDrawer} closed={sideDrawerClosedHandler}/> */}
             <section className='py-[20px] bg-back-white'>
                 <div className='big_container'>
                     <div className='flex items-center gap-2 mb-5'>
@@ -155,24 +159,37 @@ const PdpSec = ({ product }) => {
                 </div>
 
             </section>
-         
-         
-            <div id="defaultModal" tabindex="-1" aria-hidden="true" className={`fixed bg-gray-600 bg-opacity-80 top-0 left-0 right-0 z-50  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[100%] max-h-full ${preview== true? "justify-center items-center flex":"hidden "}`}>
+
+
+            <div id="defaultModal" tabindex="-1" aria-hidden="true" className={`fixed bg-gray-600 bg-opacity-80 top-0 left-0 right-0 z-50  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[100%] max-h-full ${preview == true ? "justify-center items-center flex" : "hidden "}`}>
                 <div class="relative w-full max-w-2xl max-h-full">
                     <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                         <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                          
-                            <button type="button" class="preview_close_btn text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal" onClick={(e)=>{setPreview(false)}} >
+
+                            <button type="button" class="preview_close_btn text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal" onClick={(e) => { setPreview(false) }} >
                                 <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                                 <span class="sr-only">Close modal</span>
                             </button>
                         </div>
-                    
-                        <div class="p-6 space-y-6">
-                            <Image src={`http://localhost:7777/upload/${product?.fullimages[0].filename}`} width={900} height={7570} alt='preview image' ></Image>
-                        </div>
-                      
-                     
+                        <Swiper
+                            // pagination={{
+                            //     type: "progressbar",
+                            // }}
+                            navigation={true}
+                            modules={[Pagination, Navigation]}
+                            className="mySwiper"
+                        >
+                            <div class="p-4 space-y-6">
+                                {
+                                    product?.fullimages?.map((elem, ind) => {
+                                    return    <SwiperSlide>  <Image src={`http://localhost:7777/upload/${elem.filename}`} width={1000} height={7570} alt='preview image' ></Image></SwiperSlide>
+                                    })
+
+                                }
+
+                            </div>
+                        </Swiper>
+
                     </div>
                 </div>
             </div>
--- components/new-product-Detail/slugRating.js
+++ components/new-product-Detail/slugRating.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Image from 'next/image'
 
 import star from 'public/icons/star.svg'
+import Link from 'next/link'
 const SlugRating = () => {
     return (
         <>
             <section className='bg-back-white'>
                 <div className='big_container'>
                     <div className='flex justify-between items-center py-[42px] border-b border-divider-main'>
-                        <div className='main-info'><span className='pr-2'>Home</span>/ <span className='pl-2'>Products</span></div>
+                        <div className='main-info'><Link href="/" className='pr-2'>Home</Link>/ <span className='pl-2'>Products</span></div>
                         <div className='flex gap-[10px] items-center'>
                             <span>4.9</span>
                             <Image src={star} width={20} height={20} alt="Icon" className='' />
